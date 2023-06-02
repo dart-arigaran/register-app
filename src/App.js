77c4ff3ec0322acc6910ff89061b19223366f08c
dart-arigaran <arigaran@dartinnovations.net>
2023-06-02 17:44:26 +0530

    navbar changes

--- src/App.js
+++ src/App.js
@@ -11,27 +11,29 @@ import Add_Employee from "./components/Pages/Add_Employee";
 import Update_Employee from "./components/Pages/Update_Employee";
 import View from "./components/Pages/View";
 import Model from "./components/Pages/Model";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <div className="App">
         <Router>
+          <Dashboard />
           <Routes>
-            <Route element={<Dashboard />}></Route>
-            <Route exact path="/" element={<Dashboard />} />
+            <Route exact path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/details" element={<Employee_details />} />
             <Route path="/addemployee" element={<Add_Employee />} />
             <Route path="/updateemployee/:id" element={<Update_Employee />} />
             <Route path="/viewemployee/:id" element={<View />} />
-            <Route path="/model" element={<Model />} />
+            <Route path="/model/id" element={<Model />} />
 
             <Route path="*" element={<No_Page />} />
           </Routes>
+          <Footer />
         </Router>
       </div>
     </>
