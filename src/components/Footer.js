5f94cf765b6c86da21e12ac28225f04bb8f42614
dart-arigaran <arigaran@dartinnovations.net>
2023-06-02 17:43:25 +0530

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h3>Footer</h3>
+      <h2>The copyright &copy; 2023 ,DEAD POOL ........</h2>
+    </div>
+  );
+}
+
+export default Footer;
